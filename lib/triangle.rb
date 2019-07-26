2bb851298c0089c6f18ba45ee9578d532e91f166
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-26 17:49:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,7 @@ class Triangle
   end
   
   def valid 
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-      raise TriangleError
-    elsif @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 ||  @side2 + @side3 <= @side1
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 ||  @side2 + @side3 <= @side1
       raise TriangleError
     end
   end
